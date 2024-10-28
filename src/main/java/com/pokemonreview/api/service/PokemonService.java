ab64dc4c4a616fb733b37c142b5bfb96ea3e0b29
Nguyễn Việt Hòa <84431330+Hoanguyen-2002@users.noreply.github.com>
2024-10-28 21:50:42 +0700

    change name of models folder to entity

--- src/main/java/com/pokemonreview/api/service/PokemonService.java
+++ src/main/java/com/pokemonreview/api/service/PokemonService.java
@@ -2,9 +2,6 @@ package com.pokemonreview.api.service;
 
 import com.pokemonreview.api.dto.PokemonDto;
 import com.pokemonreview.api.dto.PokemonResponse;
-import com.pokemonreview.api.models.Pokemon;
-
-import java.util.List;
 
 public interface PokemonService {
     PokemonResponse getAllPokemon(int pageNo, int pageSize);
